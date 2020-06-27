486a2dcd825ee22f5cb8bf1f102b91a41c1e8b7a
sondluu <son.d.luu@gmail.com>
2020-06-27 08:26:31 -0400

    added sudo code comments

--- public/sketch.js
+++ public/sketch.js
@@ -29,11 +29,11 @@ function preload() {
 function setup() {
 
   createCanvas(800,800);
-  camera_1 = createCapture(VIDEO);
-  camera_1.size(200,100);
+  camera_1 = createCapture(VIDEO); // create web camera named "camera1"
+  camera_1.size(200,100); // create camera frame
 
-  camera_1.hide()
-  camButton = document.getElementById("camera1");
+  camera_1.hide() //hide the camera view at first
+  camButton = document.getElementById("camera1");  // create camera button called "camera1"
 
   detector = ml5.objectDetector('yolo', modelReady)  //activate the ml5 Object Detection machine learning model
   
@@ -45,37 +45,35 @@ function setup() {
 
 }
 
-function newDrawing(data){
+function newDrawing(data){  // function to show kitty image
   noStroke();
   fill(200,0,100);
-  image(kitty, 800 - data.x*4, data.y*4, data.w/4, data.h/4);
+  image(kitty, 800 - data.x*4, data.y*4, data.w/4, data.h/4); // parse kitty image, create a frame for the kitty image
 }
 
 
 function modelReady() {
   console.log('model loaded')  
-  detect(); //function modelReady to load the modeal and initiate the detect objects by calling the "detect" funtion
+  detect(); //function modelReady to load the model and initiate the detect objects by calling the "detect" funtion
 }
 
 function detect() {
-  detector.detect(camera_1, gotResults); 
+  detector.detect(camera_1, gotResults); //detection starts getting pixels from web cam and call got Results function
 }
 
-function gotResults(err, results) {
+function gotResults(err, results) {  // function gotResults: if there's an error, then log it, otherwise store results in variable detections and continue to detect
   if (err) {
     console.log(err);
     return
   }
 
-  detections = results;
-
-  detect();    
-
+  detections = results;  //variable detections store results
+  detect();    //call function detect
 }
   
   
 function showCam(){
-camState=!camState;
+camState=!camState;    //function showCam sets the camState to be true
 }
 
 
@@ -92,10 +90,10 @@ function draw() {
   //to flip the image
   scale(-1, 1);
 
-  cam = image(camera_1,width/2-100,cam_y);
-  camButton.onclick = showCam; 
+  cam = image(camera_1,width/2-100,cam_y);  //variable cam contains image that captures from web cam
+  camButton.onclick = showCam; //onclick  camera button to show camera to show webcam view
   
-  if (camState){
+  if (camState){     //if camState is true 
     cam_y = 5;}
   else{
       cam_y = -220;
@@ -104,30 +102,30 @@ function draw() {
   
   time++;
   
-  if (detections) {
-    detections.forEach(detection => {
+  if (detections) {    //if able to detect something
+    detections.forEach(detection => {   //then for each detection, draw the following
       fill(0);
       stroke(0);
       strokeWeight(1);
       textSize(18);
-      text(detection.label, 800-detection.x*4 + 10, detection.y*4-10);
+      text(detection.label, 800-detection.x*4 + 10, detection.y*4-10);  //put lable of what category detected
       
       noFill();
       strokeWeight(3);
       stroke(0, 255, 0);
-      if (detection.label == 'person') {
-        personstate = 1;
-        personlocalstate += 1;
-        console.log('Sending:' + detection.x + ',' + detection.y+ ',' + detection.width+ ',' + detection.height);
-        var data = {
+      if (detection.label == 'person') {     //if the detected category is a person 
+        personstate = 1;   //set variable personstate to 1
+        personlocalstate += 1;   //???
+        console.log('Sending:' + detection.x + ',' + detection.y+ ',' + detection.width+ ',' + detection.height);  //sending location of the detection
+        var data = {         // create a data object that contains location of detections
          x: detection.x,
          y: detection.y,
          w: detection.width,
          h: detection.height
         }
-        socket.emit('mouse', data); 
+        socket.emit('mouse', data);   //send data object that has locations of detections to server
       
-        image(kitty, 800-detection.x*4, detection.y*4, detection.width/4, detection.height/4);    
+        image(kitty, 800-detection.x*4, detection.y*4, detection.width/4, detection.height/4);  // if detection is a person, also show the kitty image
         
       }
         persontime1++;
